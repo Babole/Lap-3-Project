ce81839cf6ea235db715cfb27c0cee41fb445af5
Stefan Mircea <stefancmircea@gmail.com>
2022-10-11 11:13:48 +0100

    fix conflicts

--- db/migrations/1_create_users.sql
+++ db/migrations/1_create_users.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id serial PRIMARY KEY,
-    name varchar(100) NOT NULL,
+    name varchar(100) NOT NULL UNIQUE,
     password varchar(100) NOT NULL
 );
