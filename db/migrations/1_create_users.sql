e1b32c780d7079ef79d1c3a21aaeb90953089807
Stefan Mircea <stefancmircea@gmail.com>
2022-10-13 23:35:04 +0100

    add wins column to users

--- db/migrations/1_create_users.sql
+++ db/migrations/1_create_users.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id serial PRIMARY KEY,
     name varchar(100) NOT NULL UNIQUE,
-    password varchar(100) NOT NULL
+    password varchar(100) NOT NULL,
+    wins int
 );
